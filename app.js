d79bfb69e88552921334051bb20992394a82ca23
Richa Kumari <115270761+Richadas123@users.noreply.github.com>
2025-12-28 12:02:28 +0530

    Update end date in countdown timer to 2026

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const endDate = "26 October 2025 8:00 PM"
+const endDate = "26 October 2026 8:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input");
@@ -21,6 +21,7 @@ clock()
 setInterval(()=>{
     clock();
 },1000)
+
 
 
 
